bf0235429e1fb93bc99335ff8cac19c6cd4f6abd
Aldrin <aldrin@plamarusa.com>
2025-04-05 17:39:18 -0700

    Initial Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,11 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
+import {cn}
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const fontSans = Plus_Jakarta_Sans({
+  variable: '--font-sans',
+  weight: ['200', '300', '400', '500', '600', '700'],
   subsets: ['latin'],
 })
 
@@ -24,11 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${fontSans.variable} antialiased`}>{children}</body>
     </html>
   )
 }
